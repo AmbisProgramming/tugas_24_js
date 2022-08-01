de7d845658ac59f6f2a099e926c681cfa268c64f
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-01 13:15:25 +0700

    Add files via upload

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+let array = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+console.log("Sebelumnya : ", array);
+
+let ascending = array.sort();
+console.log("Ascending : ", ascending);
+
+let descending = array.reverse();
+console.log("Descending : ", descending);
